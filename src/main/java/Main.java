93b281a27ed3a108b74153956994f34662d580e9
dupree719 <isaiahdup7@gmail.com>
2022-09-21 19:15:16 -0400

    Accountbalance

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Account account = new Account();
+        account.setBalance(2.00);
+        System.out.println(account.getBalance());
+    }
+}
